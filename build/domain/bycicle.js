6a5ead6158d725e11b8c725af5e83313c69452a5
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-05-22 18:11:32 +0200

    build: first commit with dependencies, testing and typescript configuration

---
+++ build/domain/bycicle.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Bycicle = void 0;
+class Bycicle {
+    constructor(id) {
+        this.id = id;
+    }
+    getId() {
+        return this.id;
+    }
+}
+exports.Bycicle = Bycicle;
