bbf101d434a6ff286e49ef982eb5350f84be5c1c
eze <erosarosario@cifpfbmoll.eu>
2022-05-23 08:37:42 +0200

    test: basics tests to check jest

--- tests/unit/bycicle.test.ts
+++ tests/unit/bycicle.test.ts
@@ -1,6 +1,11 @@
 import { Bycicle } from "../../src/domain/bycicle";
+
 const bycicle = new Bycicle(1);
 
-test('das 1', () => {
+test('should return 1', () => {
     expect(bycicle.getId()).toBe(1);
-})
+});
+
+test('print ID', () => {
+    expect(bycicle.toString()).toBe("ID: 1");
+});
